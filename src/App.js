d6196fb6fafb60c4ea3b1a66aeda7823a8404cfc
Narendra Maurya <120191897+Narennnnn@users.noreply.github.com>
2023-07-28 04:05:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,16 +6,24 @@ function App() {
   const [quote, setQuote] = useState("");
   const [anime, setAnime] = useState("");
   const [character, setCharacter] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   async function fetchDataQuote() {
     try {
+      setIsLoading(true); // Set loading state to true before making the request
       const response = await fetch("https://animechan.xyz/api/random");
-      const data = await response.json();
-      setQuote(data.quote);
-      setAnime(data.anime);
-      setCharacter(data.character);
+      if (response.ok) {
+        const data = await response.json();
+        setQuote(data.quote);
+        setAnime(data.anime);
+        setCharacter(data.character);
+      } else {
+        console.error('Error fetching data:', response.statusText);
+      }
     } catch (error) {
       console.error('Error fetching data:', error);
+    } finally {
+      setIsLoading(false); // Set loading state to false after the request is done
     }
   }
 
@@ -25,11 +33,17 @@ function App() {
 
   return (
     <div className='App'>
-      <div className="icon-container" onClick={fetchDataQuote}>
+      <div className="icon-container" onClick={fetchDataQuote} disabled={isLoading}>
         <MousePointer2 className="icon" />
       </div>
-      <h2 style={{ fontFamily: 'Abril Fatface, cursive' }}>"{quote}"</h2>
-      <h3 style={{ fontFamily: 'Abril Fatface, cursive' }}>~{character}</h3>
+      {isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          <h2 style={{ fontFamily: 'Abril Fatface, cursive' }}>"{quote}"</h2>
+          <h3 style={{ fontFamily: 'Abril Fatface, cursive' }}>~{character}</h3>
+        </>
+      )}
     </div>
   );
 }
